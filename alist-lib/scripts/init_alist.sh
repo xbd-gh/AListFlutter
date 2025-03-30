2154082493555e6f4282621bc50a2973ca7a97ff
xbd-gh <sun1109n@163.com>
2025-03-30 12:52:11 +0800

    modify sh

--- alist-lib/scripts/init_alist.sh
+++ alist-lib/scripts/init_alist.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-GIT_REPO="https://github.com/alist-org/alist.git"
-TAG_NAME=$(git -c 'versionsort.suffix=-' ls-remote --exit-code --refs --sort='version:refname' --tags $GIT_REPO | tail --lines=1 | cut --delimiter='/' --fields=3)
-
+GIT_REPO="https://github.com/xbd-gh/alist.git" #"https://github.com/alist-org/alist.git"
+#TAG_NAME=$(git -c 'versionsort.suffix=-' ls-remote --exit-code --refs --sort='version:refname' --tags $GIT_REPO | tail --lines=1 | cut --delimiter='/' --fields=3)
+TAG_NAME="xor"
 echo "AList - ${TAG_NAME}"
 rm -rf ./src
 unset GIT_WORK_TREE
-git clone --branch "$TAG_NAME" https://github.com/alist-org/alist.git ./src
+git clone --branch "$TAG_NAME" $GIT_REPO ./src
 rm -rf ./src/.git
 
 mv -f ./src/* ../
 rm -rf ./src
 
 cd ../
-go mod edit -replace github.com/djherbis/times@v1.6.0=github.com/jing332/times@latest
+#go mod edit -replace github.com/djherbis/times@v1.6.0=github.com/jing332/times@latest
